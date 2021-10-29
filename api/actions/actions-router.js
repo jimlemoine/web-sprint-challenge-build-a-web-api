47952a4a4c02a02597873d8474cf495f490c76a2
jimlemoine <jlemoine007@gmail.com>
2021-10-29 10:38:51 -0700

    actions gets working in postman

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,26 @@
 // Write your "actions" router here!
+const express = require('express');
+const {
+    handleError,
+    checkActionId,
+    validateAction,
+} = require('./actions-middlware')
+const Actions = require('./actions-model');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    Actions.get()
+        .then(actions => {
+            res.status(200).json(actions);
+        })
+        .catch(next);
+});
+
+router.get('/:id', checkActionId, (req, res, next) => {
+    res.status(200).json(req.action)
+});
+
+router.use(handleError);
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const projectsRouter = require('./projects/projects-router.js');
-
+const actionsRouter = require('./actions/actions-router.js');
 
 const server = express();
 
 server.use(express.json());
 
 server.use('/api/projects', projectsRouter);
-
+server.use('/api/actions', actionsRouter);
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
